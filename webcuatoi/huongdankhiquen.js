1cb2cd428496d506205b2a741b3722bcb4f5acb9
cuabronze <huy53708@gmail.com>
2023-05-04 07:47:38 +0700

    casestudy

---
+++ webcuatoi/huongdankhiquen.js
@@ -0,0 +1,154 @@
+// *{
+//     margin: 0;
+//     padding: 0;
+// }
+// .flex-container {
+//     display: flex;
+//     width: 100%;
+//     height: 60px;
+//     justify-content: space-between;
+//     background-color: rgba(14, 110, 52, 0.058);
+//     margin-bottom: 20px;
+//     z-index: 2;
+//     position: fixed;
+//     top: 0;
+// }
+  icon genshin
+//   .city_icon{
+//     width: 12vh;
+// }
+button ĐĂNG NHẬP
+//   .btn-ĐN{
+//     padding: 10px;
+//     width: 200px;
+//     height: 60px;
+//     background-color: #1a22275a;
+//     color: rgb(246, 10, 10);
+//     font-size: 25px;
+//     }
+//     .btn-ĐK{
+//         padding: 10px;
+//         width: 200px;
+//         height: 60px;
+//         background-color: #1a22275a;
+//         color: rgb(246, 10, 10);
+//         font-size: 25px;
+//         }
+Cuabronze shadow 
+//     .guide__title{
+//         display: grid;
+//         align-items: center;
+//         justify-content: center;
+//         font-weight: bold;
+//         color: #fff;
+//         text-shadow: 0 0 10px #f36a02, 0 0 20px #69e0ff, 0 0 40px #69e0ff;
+//     }
+
+
+
+
+button
+/* .container .search {
+    width: 50%;
+    display: flex;
+    margin-top: 25px;
+}
+
+.container .search .searchbox {
+    padding: 10px;
+    width: 400px;
+    height: 41px;
+    box-sizing: border-box;
+    border: none;
+}
+
+.container .search .searchbutton {
+    width: 70px;
+    height: 40px;
+    box-sizing: border-box;
+    margin-left: -5px;
+    border: none;
+}
+
+.container .title {
+    width: 20%;
+    background-color: yellow;
+}
+
+h1 {
+    font-weight: bold;
+    font-size: 20px;
+}
+
+.container_1 {
+    max-width: 1200px;
+    margin: auto;
+}
+
+.items {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    text-align: center;
+    margin-top: 20px;
+}
+
+.item {
+    width: 19%;
+    background-color: #fff;
+    padding: 12px;
+    text-align: center;
+} */
+
+/* img {
+    width: 200px;
+}
+
+button {
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 5px;
+    width: 100px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: antiquewhite;
+}
+
+.container-search {
+    border: 2px solid rgb(61, 55, 55);
+    height: 44px;
+    border-radius: 5px;
+}
+
+.item:hover {
+    border: 1px solid gray;
+    border-radius: 5px;
+    box-sizing: border-box;
+    transform: scale(1.05);
+    cursor: pointer;
+    border: none;
+    box-shadow: 4px 4px 2px 2px #888888;
+    cursor: pointer;
+} */
+
+/* .img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+#label1{
+    
+    margin:0 auto;
+    width:20%;
+
+}
+#labelcon1{
+    width: 200px;
+    height: 100px;
+    margin-left: -225px;
+    margin-top: -180px;
+} */
